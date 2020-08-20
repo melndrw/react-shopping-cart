d248ae59303b672b88b7c882c1a1f63e92b086b0
Mel <mel.ndrw@gmail.com>
2020-08-20 17:41:53 +0800

    create product components

---
+++ src/components/Products.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import formatCurrency from './util';
+
+const Products = (props) => {
+  return (
+    <div>
+      <ul className="products">
+        {props.products.map((product) => {
+          return (
+            <li key={product._id}>
+              <div className="product">
+                <a href={'#' + product._id} className="product__image">
+                  <img src={product.image} alt={product.title} />
+                  <p>{product.title}</p>
+                </a>
+                <div className="product__price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button className="button primary">Add To Cart</button>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
