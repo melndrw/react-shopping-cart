299827a67440c1874a839e93b5c401ca8456b1e5
Mel <mel.ndrw@gmail.com>
2020-08-23 09:29:33 +0800

    added redux on filters and conncect with database

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,10 +1,55 @@
-import { FETCH_PRODUCTS } from '../types';
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+} from '../types';
+// import products from '../data';
 
+// reducer handler for grabbing the data from the database
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch('/api/products');
   const data = await res.json();
   dispatch({
     type: FETCH_PRODUCTS,
     payload: data,
+  });
+};
+
+// reducer handler for filtering the products by size
+export const filterProducts = (products, size) => (dispatch) => {
+  dispatch({
+    type: FILTER_PRODUCTS_BY_SIZE,
+    payload: {
+      size: size,
+      items:
+        size === ''
+          ? products
+          : products.filter((x) => x.availableSizes.indexOf(size) >= 0),
+    },
+  });
+};
+
+// reducer handler for filtering the products by price
+export const sortProducts = (sortFilter, sort) => (dispatch) => {
+  const sortedProducts = sortFilter.slice();
+  if (sort === '') {
+    sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+  } else {
+    sortedProducts.sort((a, b) =>
+      sort === 'lowest'
+        ? a.price > b.price
+          ? 1
+          : -1
+        : a.price > b.price
+        ? -1
+        : 1
+    );
+  }
+  dispatch({
+    type: ORDER_PRODUCTS_BY_PRICE,
+    payload: {
+      sorts: sort,
+      items: sortedProducts,
+    },
   });
 };
--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,9 +1,28 @@
-import { FETCH_PRODUCTS } from '../types';
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+} from '../types';
 
 export const productsReducer = (state = {}, action) => {
   switch (action.type) {
+    // reducer for filtering based on sizes
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        filteredItems: action.payload.items,
+        size: action.payload.size,
+      };
+    // reducer for filtering based on prices
+    case ORDER_PRODUCTS_BY_PRICE:
+      return {
+        ...state,
+        sorts: action.payload.sorted,
+        filteredItems: action.payload.items,
+      };
+    // reducer for fetching the products data from the database
     case FETCH_PRODUCTS:
-      return { items: action.payload };
+      return { items: action.payload, filteredItems: action.payload };
     default:
       return state;
   }
